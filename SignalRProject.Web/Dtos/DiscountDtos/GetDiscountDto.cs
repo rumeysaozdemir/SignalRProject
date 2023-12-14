401062902c9090006dff22c54d69402d07d04ede
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-12-14 17:21:22 +0300

    14.12.2023

---
+++ SignalRProject.Web/Dtos/DiscountDtos/GetDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRProject.Web.Dtos.DiscountDtos
+{
+    public class GetDiscountDto
+    {
+        public int DiscountId { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

deaa285ba6606fbd4dc25d81bd0f550d8f745994
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-12-16 20:48:44 +0300

    16.12.2023

---
+++ SignalRProject.Web/Dtos/SocialMediaDtos/CreateSocialMediaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRProject.Web.Dtos.SocialMediaDtos
+{
+    public class CreateSocialMediaDto
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}

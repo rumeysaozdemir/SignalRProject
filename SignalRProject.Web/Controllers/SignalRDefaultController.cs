c28d827b63941182e677856bac17de43d05f3553
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-12-18 21:53:58 +0300

    18.12.2023

---
+++ SignalRProject.Web/Controllers/SignalRDefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.Web.Controllers
+{
+	public class SignalRDefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	
+	}
+}

c058d640d5784a0adcf72516913954a77ac7f6af
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2024-01-02 17:05:52 +0300

    02.01.2024

--- SignalRProject.Api/Hubs/SignalRHub.cs
+++ SignalRProject.Api/Hubs/SignalRHub.cs
@@ -1,8 +1,35 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRProject.BusinessLayer.Abstract;
 
 namespace SignalRProject.Api.Hubs
 {
 	public class SignalRHub : Hub
 	{
-	}
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+
+        public async Task SendStatistic()
+        {
+            var value = _categoryService.CategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+            var value2 = _productService.ProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.ActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.PassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+        }
+
+           
+
+    }
 }

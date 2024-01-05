2ba312bff8d69287a2515560dc4612aa51a6a355
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2024-01-05 22:48:37 +0300

    05.01.2024

---
+++ SignalRProject.Web/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.Web.ViewComponents.DefaultComponents
+{
+    public class _DefaultOurMenuComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRProject.Web/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.Web.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

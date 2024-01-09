74db1c93d4f8df152b635f8670ead785bbcb14d4
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2024-01-09 21:47:28 +0300

    09.01.2024

---
+++ SignalRProject.Web/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRProject.Web.Dtos.ProductDtos;
+
+namespace SignalRProject.Web.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7296/api/Product");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
